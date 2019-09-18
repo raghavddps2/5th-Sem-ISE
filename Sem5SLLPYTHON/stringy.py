a4606cf2845dfecc8df28b17c12707bc0d33dc96
Nimish Bongale <nimishnb98@gmail.com>
2019-09-18 10:17:10 +0530

    Update stringy.py

--- Sem5SLLPYTHON/stringy.py
+++ Sem5SLLPYTHON/stringy.py
@@ -1,4 +1,4 @@
-str2 = ["Hello 123!","This","is","Nimish"]
+str2 = ["Hello 123!","This","is","Nimish123"]
 strrev = []
 for i in range(0,4):
     if(i%2==0):
@@ -13,11 +13,9 @@ for i in str2:
 print(strrev)
 
 for i in str2:
-    if i.isdigit():
-        for j in i:
-            if j.isdigit():
-                print(j)
-    print()
-
-
+    if any(char.isdigit() for char in i):
+        for char in i:
+            if(str(char).isdigit()):
+                print(char)
+        print()
 
