671579459c342fc0f5ee8783fea3ffddc5afaad9
Nimish Bongale <nimishnb98@gmail.com>
2019-11-20 10:39:31 +0530

    Add files via upload

---
+++ Sem5SLLPYTHON/Flask/demo/flask123.py
@@ -0,0 +1,20 @@
+from flask import Flask,redirect,url_for,render_template, request
+app=Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('indexJS.html')
+
+@app.route('/log_in',methods = ['POST','GET'])
+def log_in():
+   if  request.method == 'POST':
+        if request.form['usn'] =='admin' and request.form['pass']=='admin':
+           return redirect(url_for('success'))
+        else:
+            return render_template('indexJS.html',msg='incorrect entry')
+            return redirect(url_for('index'))
+
+@app.route('/success')
+def success():
+   return 'logged in successfully'
+if __name__ == '__main__':
+    app.run(debug=True)
